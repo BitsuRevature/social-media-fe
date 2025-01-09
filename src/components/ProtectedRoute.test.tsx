9d9bbeff8781dd817dc209a96521f5e6d456fa63
ao donovan <a.o.donovan.music@gmail.com>
2025-01-09 12:35:58 -0500

    add test case for protected route containing valid auth store info

--- src/components/ProtectedRoute.test.tsx
+++ src/components/ProtectedRoute.test.tsx
@@ -9,18 +9,19 @@ import {
 } from "@mui/joy";
 
 
-const preloadedState = {
-    auth: null
-    }
-  const testStore = configureStore({
-    reducer: ()=>null,
-    preloadedState, 
-  });
-
   
   describe('Protected route', () => {
     it('does not render without user info from auth store', () => {
-      expect(() => render(
+
+        const preloadedState = {
+            uth: null
+            }
+        const testStore = configureStore({
+            reducer: ()=>null,
+            preloadedState, 
+        }); 
+
+    expect(() => render(
         <CssVarsProvider>
         <Provider store={testStore}>
           <Router>
@@ -29,6 +30,44 @@ const preloadedState = {
         </Provider>
         </CssVarsProvider>
       )).toThrow();
+    });
+
+    it('renders a protected route with valid user info from auth store', () => {
+
+        const preloadedState = {
+            auth: {
+              auth: {
+                token: null,
+                id: null,
+                username: null,
+                profilePicture: null,
+                bio: null,
+                firstname: "test",
+                lastname:  "testerson",
+                exipreDate: undefined
+              },
+              isLoading: false, 
+            },
+          };
+          
+          const testStore = configureStore({
+            reducer: {
+              auth: authReducer,
+            },
+            preloadedState, 
+          });
+
+        render(
+            <CssVarsProvider>
+            <Provider store={testStore}>
+            <Router>
+                <ProtectedRoute />
+            </Router>
+            </Provider>
+            </CssVarsProvider>
+        )
+        const fn_ln = document.querySelector("[data-testid=fn_ln]");
+        expect(fn_ln!.textContent).toBe("test testerson");
     });
   });
   
