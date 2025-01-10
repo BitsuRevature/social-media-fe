4db511de1863a85a67708fc2e45f3d8593952405
ao donovan <a.o.donovan.music@gmail.com>
2025-01-10 18:23:09 -0500

    searchbar smoke test

---
+++ src/components/Searchbar.test.tsx
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import SearchBar from './SearchBar';
+import {
+  CssVarsProvider,
+} from "@mui/joy";
+import { testStore } from '../../testMocks'
+
+describe('Post component', () => {
+  it('renders searchbar and does not crash', () => {
+    render(
+      <CssVarsProvider>
+      <Provider store={testStore}>
+        <Router>
+          <SearchBar onChange={()=>console.log("change fn")}/>
+        </Router>
+      </Provider>
+      </CssVarsProvider>
+    );
+  });
+});
