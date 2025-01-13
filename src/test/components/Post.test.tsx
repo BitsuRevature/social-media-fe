96352e51532af18ba93b2296ed927fb36fecdfa5
Ethan Tang <itsethang5@gmail.com>
2025-01-13 10:43:46 -0500

    Create tests for testing post user interactions

--- src/test/components/Post.test.tsx
+++ src/test/components/Post.test.tsx
@@ -1,24 +1,52 @@
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Post from '../../components/Post';
-import {
-  CssVarsProvider,
-} from "@mui/joy";
-import { testStore, mockPost } from '../testMocks'
+import { CssVarsProvider } from "@mui/joy";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import Post from "../../components/Post";
+import { mockPost, testStore } from "../testMocks";
 
-describe('Post component', () => {
-  it('renders post component with supplied mock data', () => {
+describe("Post component", () => {
+  const renderComponent = () => {
     render(
       <CssVarsProvider>
-      <Provider store={testStore}>
-        <Router>
-          <Post post={mockPost}/>
-        </Router>
-      </Provider>
+        <Provider store={testStore}>
+          <Router>
+            <Post post={mockPost} />
+          </Router>
+        </Provider>
       </CssVarsProvider>
     );
+
+    return {
+      user: userEvent.setup(),
+      addIcon: screen.getByTestId("AddRoundedIcon"),
+      likeIcon: screen.getByTestId("FavoriteIcon"),
+      removeIcon: screen.findByTestId("RemoveIcon"),
+      commentInput: screen.findByPlaceholderText(/new comment/i),
+    };
+  };
+
+  it("renders post component with supplied mock data", () => {
+    renderComponent();
     const pc = document.querySelector("[data-testid=postContent]");
     expect(pc!.textContent).toBe("mock post content");
+  });
+
+  it("should render all the post interaction actions", () => {
+    const { addIcon, likeIcon } = renderComponent();
+    expect(addIcon).toBeInTheDocument();
+    expect(likeIcon).toBeInTheDocument();
+  });
+
+  it("should open up the comment input box and change add icon to remove icon when add icon is clicked", async () => {
+    const { user, addIcon, commentInput, removeIcon } = renderComponent();
+    user.click(addIcon);
+    const commentBox = await commentInput;
+    const removeAction = await removeIcon;
+
+    expect(commentBox).toBeInTheDocument();
+    expect(removeAction).toBeInTheDocument();
   });
 });
--- test.setup.ts
+++ test.setup.ts
@@ -1,12 +1,12 @@
-import "@testing-library/jest-dom/vitest"
-import { afterEach} from 'vitest'
-import { cleanup } from '@testing-library/react'
-import { vi } from 'vitest'
+import "@testing-library/jest-dom/vitest";
+import { afterEach } from "vitest";
+import { cleanup } from "@testing-library/react";
+import { vi } from "vitest";
 
 //this property has to be defined manually for some reason
-Object.defineProperty(window, 'matchMedia', {
+Object.defineProperty(window, "matchMedia", {
   writable: true,
-  value: vi.fn().mockImplementation(query => ({
+  value: vi.fn().mockImplementation((query) => ({
     matches: false,
     media: query,
     onchange: null,
@@ -16,9 +16,9 @@ Object.defineProperty(window, 'matchMedia', {
     removeEventListener: vi.fn(),
     dispatchEvent: vi.fn(),
   })),
-})
+});
 
 // runs a clean after each test case (e.g. clearing jsdom)
 afterEach(() => {
   cleanup();
-})
+});
