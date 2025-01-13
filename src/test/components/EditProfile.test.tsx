d1d52052e77c321ecf5f9d9a0ae613d550035c31
ao donovan <a.o.donovan.music@gmail.com>
2025-01-13 17:05:16 -0500

    change mock user id to real user id

--- src/test/components/EditProfile.test.tsx
+++ src/test/components/EditProfile.test.tsx
@@ -2,12 +2,10 @@ import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import EditProfile from '../../components/EditProfile';
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../../features/auth/authSlice';
 import {
   CssVarsProvider,
 } from "@mui/joy";
-import { preloadedState as mockUserState, testStore } from '../testMocks';
+import { testStore } from '../testMocks';
 import { assert } from 'vitest';
 
 describe('Edit Profile', () => {
